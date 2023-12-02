badb15d1f1a4fcf322254ede3049fa0a0e54e9cf
= <kairiueno2003@icloud.com>
2023-12-02 21:19:23 +0900

    end

--- twitter/vite.config.ts
+++ twitter/vite.config.ts
@@ -4,4 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    rollupOptions: {
+      output: {
+        // 他の設定...
+        // _redirectsファイルを公開ディレクトリにコピー
+        assetFileNames: assetInfo => {
+          if (assetInfo.name === '_redirects') return assetInfo.name;
+          return 'assets/[name]-[hash][extname]';
+        }
+      }
+    }
+  }
 });
