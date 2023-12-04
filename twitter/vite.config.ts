a64b396265487ccfe9d07e87919b863b2fd34d13
= <kairiueno2003@icloud.com>
2023-12-04 10:25:30 +0900

    niko

--- twitter/vite.config.ts
+++ twitter/vite.config.ts
@@ -4,17 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  build: {
-    rollupOptions: {
-      output: {
-        // 他の設定...
-        // _redirectsファイルを公開ディレクトリにコピー
-        assetFileNames: assetInfo => {
-          if (assetInfo.name === '_redirects') return assetInfo.name;
-          return 'assets/[name]-[hash][extname]';
-        }
-      }
-    }
-  }
 });
